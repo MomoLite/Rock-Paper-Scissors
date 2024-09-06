fcfd53150fd54d520141c00d9f2105c809ae8e29
Moe <sauceuptheghost613@gmail.com>
2024-09-06 12:47:38 -0400

    Logic written for the playRound() function. The human score gets incremented if they win and also the computer scores get incremented if they win.

--- index.js
+++ index.js
@@ -19,4 +19,38 @@ function getHumanChoice() {
     return prompt("What's your move? ");
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase().trim();
+
+    if (humanChoice === computerChoice) {
+        console.log("Draw! Both chose " + humanChoice + ".")
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You won! Rock beats Scissors.");
+        humanScore += 1;
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock.");
+        computerScore += 1;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You won! Paper beats Rock.");
+        humanScore += 1;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beat Paper");
+        computerScore += 1;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You won! Scissors beat Paper");
+        humanScore += 1;
+    }
+    else {
+        console.log("You lose! Rock beats Scissors.");
+        computerScore += 1;
+    }
+    console.log();
+}
+
+
 
